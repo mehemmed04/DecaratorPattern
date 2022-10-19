778fb7a131f159e0400f369f8274a3f969702cae
Bayr_kf60@itstep.edu.az <Bayr_kf60@itstep.edu.az>
2022-10-19 10:36:14 -0700

    Added pattern

--- DecaratorPattern/MainWindow.xaml.cs
+++ DecaratorPattern/MainWindow.xaml.cs
@@ -15,50 +15,172 @@ using System.Windows.Shapes;
 
 namespace DecaratorPattern
 {
-  
-
-
-
     public partial class MainWindow : Window
     {
+        class Item
+        {
+            public string Message { get; set; }
+            public string ImagePath { get; set; }
+        }
+
+        interface IService
+        {
+            List<Item> GetItems(string Message);
+        }
+
+
+        class Service : IService
+        {
+            public List<Item> GetItems(string Message)
+            {
+                return new List<Item>();
+            }
+        }
+
+        class ServiceDecarator : IService
+        {
+
+            public IService _service;
+            public ServiceDecarator(IService service)
+            {
+                _service = service;
+            }
+            public virtual List<Item> GetItems(string Message)
+            {
+                return _service.GetItems( Message);
+            }
+        }
+
+        class SmsServiceDecarator : ServiceDecarator
+        {
+            public SmsServiceDecarator(IService service) : base(service)
+            {
+
+            }
+
+            public override List<Item> GetItems(string Message)
+            {
+                Item item = new Item
+                {
+                    ImagePath = "Images/sms.png",
+                    Message = $"{Message} From SmsService"
+                };
+                var items = _service.GetItems( Message);
+                items.Add(item);
+                return items;
+            }
+        }
+
+        class FacebookServiceDecarator : ServiceDecarator
+        {
+            public FacebookServiceDecarator(IService service) : base(service)
+            {
+
+            }
+
+            public override List<Item> GetItems(string Message)
+            {
+                Item item = new Item
+                {
+                    ImagePath = "Images/facebook.png",
+                    Message = $"{Message} From FacebookService"
+                };
+                var items = _service.GetItems(Message);
+                items.Add(item);
+                return items;
+            }
+        }
+
+        class InstagramServiceDecarator : ServiceDecarator
+        {
+            public InstagramServiceDecarator(IService service) : base(service)
+            {
+
+            }
+
+            public override List<Item> GetItems(string Message)
+            {
+                Item item = new Item
+                {
+                    ImagePath = "Images/instagram.png",
+                    Message = $"{Message} InstagramService"
+                };
+                var items = _service.GetItems(Message);
+                items.Add(item);
+                return items;
+            }
+        }
+
+        class TwitterServiceDecarator : ServiceDecarator
+        {
+            public TwitterServiceDecarator(IService service) : base(service)
+            {
+
+            }
+
+            public override List<Item> GetItems(string Message)
+            {
+                Item item = new Item
+                {
+                    ImagePath = "Images/twitter.png",
+                    Message = $"{Message} From TwitterService"
+                };
+                var items = _service.GetItems(Message);
+                items.Add(item);
+                return items;
+            }
+        }
+
+
         public RelayCommand SendMessageCommand { get; set; }
+        private IService service = new Service();
+        private ServiceDecarator decarator { get; set; }
         public MainWindow()
         {
             InitializeComponent();
             this.DataContext = this;
             SendMessageCommand = new RelayCommand((e) =>
             {
+                if(SmsService.IsChecked == true)
+                {
+                    IService SmsService = new SmsServiceDecarator(decarator);
+                    service = SmsService;
+                    decarator = new ServiceDecarator(service);
+                }
+                if(FacebookService.IsChecked == true)
+                {
+                    IService FacebookService = new FacebookServiceDecarator(decarator);
+                    service = FacebookService;
+                    decarator = new ServiceDecarator(service);
+                }
+                if(InstagramService.IsChecked == true)
+                {
+                    IService InstagramService = new InstagramServiceDecarator(decarator);
+                    service = InstagramService;
+                    decarator = new ServiceDecarator(service);
+                }
+                if(TwitterService.IsChecked == true)
+                {
+                    IService TwitterService = new TwitterServiceDecarator(decarator);
+                    service = TwitterService;
+                    decarator = new ServiceDecarator(service);
+                }
+
+                List<Item> Items = decarator.GetItems(TextBox1.Text);
+                service = new Service();
+                decarator = new ServiceDecarator(service);
+
+                ItemsListView.ItemsSource = null;
+                ItemsListView.ItemsSource = Items;
 
             }, (e) =>
             {
                 return TextBox1.Text != string.Empty;
             });
 
-        }
-
-        private void CheckBox_Click(object sender, RoutedEventArgs e)
-        {
+            decarator = new ServiceDecarator(service);
 
         }
 
-        private void SmsService_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void FacebookService_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void InstagramService_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
-
-        private void TwitterService_Click(object sender, RoutedEventArgs e)
-        {
-
-        }
     }
 }
